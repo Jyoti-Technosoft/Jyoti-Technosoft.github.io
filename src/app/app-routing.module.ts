999b66413ef1a15ef0ef903be922467fddded756
Dipesh Gandhi <dipesh_gandhi9@yahoo.com>
2023-03-10 18:39:34 +0530

    Jyoti Technosoft : Add footer inside website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullLayoutComponent } from './layout/full-layout/full-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FullLayoutComponent,
+    children:[
+      
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './component/main-header/main-header.component';
+import { BlankComponent } from './layout/blank/blank.component';
+import { FullLayoutComponent } from './layout/full-layout/full-layout.component';
+import { SharedModule } from './shared-components/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FullLayoutComponent, BlankComponent, MainHeaderComponent, FooterComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    AppRoutingModule, 
+    SharedModule, BrowserAnimationsModule, 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
