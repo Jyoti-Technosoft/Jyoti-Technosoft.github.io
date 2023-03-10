61b220169fe89d7f9ff216c0b94af5c3c2caebb3
Dipesh Gandhi <dipesh_gandhi9@yahoo.com>
2023-03-10 18:41:09 +0530

    Jyoti Technosoft : Add full layout component

---
+++ src/app/shared-components/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+const materialModule = [
+  MatToolbarModule,
+  MatIconModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    materialModule
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    materialModule
+  ]
+})
+export class SharedModule { }
