1920ae67ca8cee0378e249339160c21fb186d001
Dipesh Gandhi <dipesh_gandhi9@yahoo.com>
2023-03-13 19:20:06 +0530

    Jyoti Technosoft : Add validation and icons for the contact info label

--- src/app/component/contact-us/contact-us.component.ts
+++ src/app/component/contact-us/contact-us.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -11,19 +11,30 @@ export class ContactUsComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {}
 
+  contactInfo!: {label:string, icon:string}[];
   ngOnInit(): void {
-      this.inItForm();
+      this.initForm();
+      this.contactInfo = [
+        { label: 'Address', icon: 'fa-map-marker'},
+        { label: 'email', icon: 'fa-envelope'},
+        { label: 'contact no.', icon: 'fa-phone'}
+      ]
   }
 
 
-  inItForm() {
+  initForm() {
     this.contactUsForm = this.fb.group({
-      firstName: [''],
+      firstName: ['', Validators.required],
       lastName: [''],
-      emailAddress: [''],
-      subject: [''],
-      mobileNo: [''],
-      description: [''],
+      emailAddress: ['', Validators.email],
+      subject: ['', [Validators.required, Validators.maxLength(30)]],
+      mobileNo: ['', Validators.required, Validators.minLength(10),Validators.maxLength(13)],
+      description: ['', [Validators.required, Validators.maxLength(250)]],
+      recaptcha: ['', Validators.required],
     })
+  }
+
+  resolved(captchaResponse: string) {
+    console.log(`Resolved captcha with response: ${captchaResponse}`);
   }
 }
