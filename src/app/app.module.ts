c0ba08f544e73eeda8765db5989c2c9ebeb271a9
Poojan Patel <poojan.jtdev@gmail.com>
2023-03-16 11:14:54 +0530

    JTWEB-07 :   -Implemented lazy loading for home component .   -created card and Carousel for home design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from '@app/component/main-header/main-header.component';
@@ -27,6 +29,7 @@ import { ServicesComponent } from '@app/component/services/services.component';
     AppRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnDestroy,OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  ngOnDestroy(): void {
+      
+  }
+}
