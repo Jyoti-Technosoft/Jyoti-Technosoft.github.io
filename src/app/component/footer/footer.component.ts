7e18be99d74b6a32335af85e067f38d849038a0c
prabhat thakur <prabhat.jtdev@gmail.com>
2023-03-18 11:00:53 +0530

    JYOTI TECHNOSOFT : Remove console

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -23,16 +23,7 @@ export class FooterComponent implements OnInit {
   copyright!: any;
 
   ngOnInit() {
-    console.log(data?.footer?.title)
     this.mainHeader = Object.keys(data?.footer?.title)
-    console.log(this.mainHeader);
-    console.log(this.data.footer.title.company.data)
-    // company.label
-    // this.mainHeader.map((item:any) => {
-    //   console.log(item)
-    //   this.subtitle[item] = this.data?.footer?.[item]
-    //   console.log(this.subtitle)
-    // })
     this.copyright = this.data.footer.copyright;
   }
 
