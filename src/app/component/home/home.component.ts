08305638b89e35fa358b6bf6caa4aa9ca57f7032
Charmi Chauhan <88572890+charmijtdev@users.noreply.github.com>
2023-03-27 19:31:11 +0530

    changes for header and for service file change

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -3,17 +3,55 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnDestroy,OnInit {
-
+export class HomeComponent implements OnDestroy, OnInit {
   constructor() {}
 
-  ngOnInit(): void {
-      
-  }
+  cardContent: CardContent[] = [
+    {
+      cardHeader: 'Where imagination meets creativity',
+      cardData:
+        "Hireing ",
+    },
+  ];
 
-  ngOnDestroy(): void {
-      
-  }
+  hireUsContent: hireUsContent[] = [
+    {
+      cardHeader: 'Web development',
+      cardData: 'Hire Us in Web Development .',
+    },
+    {
+      cardHeader: 'Anuglar Development',
+      cardData: 'Hire Us in Web Development .',
+    },
+    {
+      cardHeader: 'Java development',
+      cardData: 'Hire Us in Web Development .',
+    },
+    {
+      cardHeader: 'WordPress development',
+      cardData: 'Hire Us in Web Development .',
+    },
+    {
+      cardHeader: 'Laravel development',
+      cardData: 'Hire Us in Web Development .',
+    },
+    {
+      cardHeader: 'UI Desiging',
+      cardData: 'Hire Us in Web Development .',
+    },
+  ];
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {}
+}
+interface CardContent {
+  cardHeader: string;
+  cardData: string;
+}
+interface hireUsContent {
+  cardHeader: string;
+  cardData: string;
 }
