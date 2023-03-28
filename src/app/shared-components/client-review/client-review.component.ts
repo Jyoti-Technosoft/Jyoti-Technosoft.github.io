628c557876064c39bb0f8e00acca92eb29a1ce12
Prabhat Thakur <prabhat.jtdev@gmail.com>
2023-03-28 10:33:09 +0530

    JT-WEBSITE : Add resize event for client review card

--- src/app/shared-components/client-review/client-review.component.ts
+++ src/app/shared-components/client-review/client-review.component.ts
@@ -1,15 +1,55 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { HostListener } from '@angular/core';
 @Component({
   selector: 'app-client-review',
   templateUrl: './client-review.component.html',
   styleUrls: ['./client-review.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ClientReviewComponent {
 
-  reviewItem:any[] = [10, 20, 30, 40, 50];
+export class ClientReviewComponent implements OnInit, AfterContentInit  {
+  reviewItem: any[] = [1,2,3,4,5];
+  cardPerPage: number = 2;
+  currentIndex: number = 0;
+  reviewPage: number = 0;
+  counter = new Array();
 
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  constructor() {}
 
+  ngOnInit() {
+    this.getRowforReviewCard();
+  }
+
+  ngAfterContentInit () {
+    this.onResize();
+  }
+
+  getRowforReviewCard() {
+    this.counter = new Array(
+      Math.ceil(Number(this.reviewItem?.length / this.cardPerPage))
+    );
+  }
+
+  onSlide(event: any) {
+    this.currentIndex = event.current;
+  }
+
+  @HostListener('window:resize') onResize() {
+    let width = window.innerWidth;
+    switch (true) {
+      case width < 980:
+        this.cardPerPage = 1;
+        break;
+      default:
+        this.cardPerPage = 2;
+        break;
+    }
+    this.getRowforReviewCard();
+  }
 }
