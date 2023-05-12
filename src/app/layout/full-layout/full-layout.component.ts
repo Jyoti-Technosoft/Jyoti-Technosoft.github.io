b982fd1216f26a9e00f11f4f231fbea9be13a01b
Prabhat Thakur <prabhat.jtdev@gmail.com>
2023-05-12 16:34:44 +0530

    -Implement scroll up button in all pages

--- src/app/layout/full-layout/full-layout.component.ts
+++ src/app/layout/full-layout/full-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-full-layout',
@@ -6,10 +7,32 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./full-layout.component.scss'],
 })
 export class FullLayoutComponent implements OnInit, OnDestroy {
+  scrollSubscription!: Subscription;
+  showBtn: boolean = false;
 
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.buttonClick();
+  }
+
+  buttonClick() {
+    this.scrollSubscription = fromEvent(document, 'scroll')
+      .pipe()
+      .subscribe(() => {
+        this.showBtn = window.scrollY > 600;
+      });
+  }
+
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
 
   ngOnDestroy(): void {
